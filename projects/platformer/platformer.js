05fc7e27c83241ef4f0ae4316331859344865553
slade2435 <sladenielsen5@gmail.com>
2024-09-05 19:50:09 +0000

    custom css

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,17 +38,18 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+createPlatform(800,650,200,0.5);
+createPlatform(500,550,200,0.5);
+createPlatform(250,450,200,0.5);
+createPlatform(700,350,200,0.5);
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
-    // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+   // example usage: createCollectable(type, x, y, gravity, bounce)
+createCollectable(800);
+createCollectable(350);
+createCollectable(110);
 
     
     // TODO 3
@@ -56,9 +57,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
+createCannon('bottom', 650, 1000,25);
+createCannon('top', 650, 1000, 25);
+createCannon('right',335, 1000, 25);
     
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
